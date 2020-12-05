e07c7da6e81ff372ac3b67181ff45b755546fc94
Aniruddha Mandal <ani96dh@gmail.com>
2020-12-06 01:18:16 +0530

    Initial file setup

---
+++ regression_model/regression_model/train_pipeline.py
@@ -0,0 +1,26 @@
+import pathlib
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
+TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
+DATASET_DIR = PACKAGE_ROOT / 'datasets'
+
+TESTING_DATA_FILE = DATASET_DIR / 'test.csv'
+TRAINING_DATA_FILE = DATASET_DIR / 'train.csv'
+
+TARGET = 'SalePrice'
+
+FEATUERS = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
+'OverallCond', 'YearRmodAdd', 'RoofStyle', 'MasVnrType', 'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir', '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual', 'Fireplaces' , 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageCars', 'PaveDrive', 'LotFrontage','YrSold']
+
+
+def save_pipeline()-> None:
+    """Persist the pipeline."""
+    pass
+
+def run_training() -> None:
+    """Train the model."""
+
+    print('Training...')
+
+if __name__ == '__main__':
+    run_training()
